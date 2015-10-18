cdf2924fff46bcfa346a41d5da89b19f5037c264
pavelecp <p.pavelec@gmail.com>
2015-10-18 15:18:49 +0200

    Added model classes.

--- semestralka/model/src/main/java/cz/cvut/fit/pavelpat/apj/semestralka/model/Order.java
+++ semestralka/model/src/main/java/cz/cvut/fit/pavelpat/apj/semestralka/model/Order.java
@@ -1,5 +1,17 @@
 package cz.cvut.fit.pavelpat.apj.semestralka.model;
 
+import cz.cvut.fit.pavelpat.apj.semestralka.model.id.OrderID;
+
 public class Order {
+
+	private final OrderID id;
+	private String foodName;
+	private double foodPrice;
+
+	public Order(OrderID id, String foodName, double foodPrice) {
+		this.id = id;
+		this.foodName = foodName;
+		this.foodPrice = foodPrice;
+	}
 
 }
